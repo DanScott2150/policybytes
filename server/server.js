6dbd8aa6a3f500407f0eed84d9f11b168a992f98
danscott2150 <danscott2150@gmail.com>
2019-03-04 13:24:07 -0500

    Linkify update pt2

--- server/server.js
+++ server/server.js
@@ -60,7 +60,7 @@ const PORT = process.env.PORT || 5000;
 //    console.log(`Listening on port: ${PORT}`);
 //  }); 
 
-app.listen is for heroku
+// app.listen is for heroku
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
 }); 
