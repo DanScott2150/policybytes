52b829662ba91de41f4ce9ac9900c72299da005c
danscott2150 <danscott2150@gmail.com>
2019-04-03 15:47:41 -0400

    Started to break out Landing Page header into separate component

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -4,55 +4,27 @@ import Footer from '../Footer/Footer.jsx'
 
 import { Panel, Grid, Col, Row, Jumbotron, Image } from 'react-bootstrap';
 
-import dummyTopicCache from '../TopicPage/DummyData.js'
 import LandingPageFeaturedTopic from './LandingPageFeaturedTopic.jsx'
 import LandingPageArchive from './LandingPageArchive.jsx'
+import LandingPageHeader from './LandingPageHeader';
 
 import './LandingPage.css'
 
 
 export class LandingPage extends Component {
 
-
-
   render() {
     return (
       <div>
-          {/* <div className="wrapper"> */}
+        <LandingPageHeader />
 
-          <Jumbotron className="landingJumbotron" >
-            <Grid>
-                  <div className="landingJumbotronInner">
-                    <Row>
-                          <Col xs={12} s={12} md={12} lg={12}>
-                              <h1><strong>The Policy Bytes Format</strong></h1>
-                          </Col>
-                    </Row>
-                    <Row>
-                          <Col xs={12} s={12} md={12} lg={12}>
-                          <p>
-                              This site is designed to facilitate better debate. 
-                              You can scan arguments and cut to the chase examining evidence in 
-                              these curated conversations. 
-                              Creating intentional dialogue that focuses on individual argumentation.
-                          </p>
-                          </Col>
-                    </Row>
-                  </div>
-            </Grid>
-          </Jumbotron>
+        <div className="dottedLine"></div>
 
-            <div className="dottedLine"></div>
+        <LandingPageFeaturedTopic />
 
-            <LandingPageFeaturedTopic />
+        <div className="dottedLine"></div>
 
-            <div className="dottedLine"></div>
-
-            <LandingPageArchive /> 
-
-
-
-          {/* </div> */}
+        <LandingPageArchive /> 
 
           <Footer/>
 
